914876cc8000f5a892faf3b82817a924b9ccf6e4
onurcankaya <onur.eren.cankaya@gmail.com>
2020-01-09 20:52:01 +0100

    added nes.css animating octocat

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './index.css'
 import Experience from './experience'
 import Skills from './skills'
+import './nes.min.css'
 
 const About = ({ content, experience, skills }) => (
   <div className="container">
@@ -10,6 +11,11 @@ const About = ({ content, experience, skills }) => (
       <div className="about-me-text">{content.aboutText}</div>
       <Experience experience={experience} />
       <Skills skills={skills} />
+      <div className="octocat-container">
+        <a href="https://github.com/onurcankaya" target="_blank" rel="noopener noreferrer">
+          <i className="nes-octocat animate"></i>
+        </a>
+      </div>
     </div>
   </div>
 )
