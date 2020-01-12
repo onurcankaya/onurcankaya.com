02711fd221783cf7108df613d3aec09b4ce67af7
onurcankaya <onur.eren.cankaya@gmail.com>
2020-01-12 09:49:34 +0100

    implemented google analytics to track page views on the home page

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
 import App from './App'
 import './index.css'
-import TagManager from 'react-gtm-module'
-
-const tagManagerArgs = {
-  gtmId: 'GTM-58VMN44',
-}
-
-TagManager.initialize(tagManagerArgs)
 
 ReactDOM.render(<App />, document.getElementById('root'))
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import ReactGA from 'react-ga'
 import PageIntro from './page-intro'
 import Projects from './projects'
+
+ReactGA.initialize('UA-149400511-2')
+ReactGA.pageview('/')
 
 const Home = ({ content, projects }) => (
   <div className="container home-page-container">
